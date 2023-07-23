f503860d6f98508d5a6aaff32bf37299939735cd
ariadne07 <119911938+ariadne07@users.noreply.github.com>
2023-07-23 15:14:53 -0400

    Create withSpaces.py

---
+++ withSpaces.py
@@ -0,0 +1,3 @@
+# This program will identify english if the decryption makes sense in English
+# "makes sense" means the words in the possible decryption are words in the English dictionary 
+# precondition: spaces are given in the encrypted message
